32104c507326dc1d172f7674b6efee004d388772
HAYASHI Kentaro <kenhys@gmail.com>
2012-03-12 01:52:49 +0900

    add -I./src

--- test/run-test.sh
+++ test/run-test.sh
@@ -5,5 +5,5 @@ basedir=`dirname $0`
 for test in $basedir/../test/test-*.scm
 do
   echo "Running test $test"
-  gosh -I. -I./lib -I$basedir/../ -I$basedir/../lib $test -vp
+  gosh -I. -I./lib -I./src -I../src -I$basedir/../ -I$basedir/../lib $test -vp
 done
